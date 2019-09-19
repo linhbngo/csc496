2eeff5a550cd40719120ddd4ace3164129166e60
Linh B Ngo <lngo@wcupa.edu>
2019-09-19 08:44:59 -0400

    Update hello.c

--- source/hello.c
+++ source/hello.c
@@ -1,28 +1,11 @@
-#include <mpi.h>
 #include <stdio.h>
-
-int main(int argc, char** argv) {
-  int size;
-  int my_rank; 
-  char proc_name[MPI_MAX_PROCESSOR_NAME];
-  int proc_name_len;
-    
-  /* Initialize the MPI environment */
+#include <sys/utsname.h>
+#include <mpi.h>
+int main(int argc, char *argv[]){
   MPI_Init(&argc, &argv);
-  
-  /* Get the number of processes */
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-  /* Get the rank of the process */
-  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-
-  /* Get the name of the processor */
-  MPI_Get_processor_name(proc_name, &proc_name_len);
-
-  /* Print off a hello world message */
-  printf("Hello world from processor %s, rank %d out of %d processes\n",
-           proc_name, my_rank, size);
-
-  /* Finalize the MPI environment. */
+  struct utsname uts;
+  uname (&uts);
+  printf("My process is on node %s.\n", uts.nodename);
   MPI_Finalize();
+  return 0;
 }
